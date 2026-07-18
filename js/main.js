879851429d336dcfa1fac09debaeaaac6a93cdc8
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-07-18 07:02:14 -0500

    Add temporary styles and scripts for menu button

--- js/main.js
+++ js/main.js
@@ -6,5 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
    console.log('DOMContentLoaded has loaded!');
 
-
+   document.querySelector('#menuButton').addEventListener('click', function () {
+      this.classList.toggle('is-open');
+   });
 });
