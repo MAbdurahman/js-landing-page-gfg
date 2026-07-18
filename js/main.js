1ad5a517d2362531843afe294b48cb553f22ec96
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2026-07-18 15:53:50 -0500

    Add temporary styles and scripts for light dark button

--- js/main.js
+++ js/main.js
@@ -6,7 +6,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
    console.log('DOMContentLoaded has loaded!');
 
+   const icon = document.getElementById('lightDarkIcon');
+   
+   document.querySelector('#lightDarkButton').addEventListener('click', function () {
+      icon.src = icon.src.includes('moon') ? './assets/img/sun.png' : './assets/img/moon.png';
+   });
+
    document.querySelector('#menuButton').addEventListener('click', function () {
       this.classList.toggle('is-open');
    });
+
+
+
+
 });
